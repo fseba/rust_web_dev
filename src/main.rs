84edc2f153d37c69b5f9491e5fec5702c1e11973
sebastianfrueh <sfrueh@gmx.net>
2024-01-25 09:36:24 +0100

    Add question struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
+use std::{io::{Error, ErrorKind}, str::FromStr};
+
 fn main() {
-    println!("Hello, world!");
+
+#[derive(Debug)]
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+}
+
+#[derive(Debug)]
+struct QuestionId(String);
+
+impl Question {
+    pub fn new(
+        id: QuestionId,
+        title: String,
+        content: String,
+        tags: Option<Vec<String>>
+    ) -> Self { Question {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
+
+impl FromStr for QuestionId {
+    type Err = std::io::Error;
+    
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(QuestionId(id.to_string())),
+            true => Err(
+                Error::new(ErrorKind::InvalidInput, "No id provided")
+            ),
+        }
+    }
+}
+
+impl std::fmt::Display for QuestionId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        write!(f, "id: {}", self.0)
+    }
+}
+
+    let question = Question::new(
+        QuestionId::from_str("1").expect("No id provided"),
+        "First Question".to_string(),
+        "Content of question".to_string(),
+        Some(vec!("faq".to_string()))
+    );
+
+    println!("{:?}", question);
 }
