4404e91282c33d2b2e55719dd869d55b5a1a8144
sebastianfrueh <sfrueh@gmx.net>
2024-01-28 16:37:35 +0100

    Add server / route

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::{io::{Error, ErrorKind}, str::FromStr};
+use serde::Serialize;
+use warp::{reject::Reject, Filter, Rejection, Reply, http::StatusCode};
 
-fn main() {
-
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 struct Question {
     id: QuestionId,
     title: String,
@@ -10,7 +10,7 @@ struct Question {
     tags: Option<Vec<String>>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 struct QuestionId(String);
 
 impl Question {
@@ -47,12 +47,51 @@ impl std::fmt::Display for QuestionId {
     }
 }
 
+#[derive(Debug)]
+struct InvalidId;
+impl Reject for InvalidId {}
+
+async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
     let question = Question::new(
-        QuestionId::from_str("1").expect("No id provided"),
-        "First Question".to_string(),
-        "Content of question".to_string(),
+        QuestionId::from_str("1").expect("No id provided"), 
+        "First Question".to_string(), 
+        "Content of question".to_string(), 
         Some(vec!("faq".to_string()))
     );
+    
+    match question.id.0.parse::<i32>() {
+        Err(_) => {
+            Err(warp::reject::custom(InvalidId))
+        },
+        Ok(_) => {
+            Ok(warp::reply::json(&question))
+        }
+    }
+}
 
-    println!("{:?}", question);
+async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
+    if let Some(_invalid_id) = r.find::<InvalidId>() {
+        Ok(warp::reply::with_status(
+            "No valid ID presented", 
+            StatusCode::UNPROCESSABLE_ENTITY))
+    } else {
+        Ok(warp::reply::with_status(
+            "Route not found", 
+            StatusCode::NOT_FOUND))
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let get_items = warp::get()
+        .and(warp::path("questions"))
+        .and(warp::path::end())
+        .and_then(get_questions)
+        .recover(return_error);
+    
+    let routes = get_items;
+
+    warp::serve(routes)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 }
